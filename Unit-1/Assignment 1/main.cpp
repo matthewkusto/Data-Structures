1997af4e3dbe7a1953bb5c1f4a37b45b4a4d34d9
matthewkusto <mkusto.2001@gmail.com>
2024-01-29 12:18:29 -0800

    Added an example run

--- Unit-1/Assignment 1/main.cpp
+++ Unit-1/Assignment 1/main.cpp
@@ -9,6 +9,16 @@ Date: 01/27/2024
 
 int main()
 {
+    // BitsByte bite;
+
+    // bite.setValue(99);
+
+    // for (int i = 0; i < 8; i++)
+    //     cout << bite.at(i) << endl;
+
+    // cout << "Int:    " << bite.toInt() << endl;
+    // cout << "String: " << bite.toString() << endl;
+
     BitsByte bite(7);
 
     BitsByte b1 = bite.add(2);

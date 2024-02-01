6297148ef5f5f7f372bd0d30947ba1ab819d1373
matthewkusto <mkusto.2001@gmail.com>
2024-01-31 22:12:47 -0800

    doxygen comments

--- Unit-2/Assignment 2/BitsWord.h
+++ Unit-2/Assignment 2/BitsWord.h
@@ -7,20 +7,43 @@ private:
     BitsByte *mUpper;
 
 public:
+    /**
+     * @brief Construct a new Bits Word object
+     *
+     */
     BitsWord();
+
+    /**
+     * @brief Construct a new Bits Word object
+     *
+     * @param value
+     */
     BitsWord(int value);
+
+    /**
+     * @brief Destroy the Bits Word object
+     *
+     */
     ~BitsWord();
-    // Mutator
+
+    /**
+     * @brief Set the Value object
+     *
+     * @param value
+     */
     void setValue(int value); // sets mUpper and mLower
-    // Accessors
+
+    /**
+     * @brief Get the Lower object
+     *
+     * @return BitsByte*
+     */
     BitsByte *getLower();
+
+    /**
+     * @brief Get the Upper object
+     *
+     * @return BitsByte*
+     */
     BitsByte *getUpper();
 };
-
-BitsWord::BitsWord(/* args */)
-{
-}
-
-BitsWord::~BitsWord()
-{
-}

18e9a646b3aa476a5817f84132b1772666ad375d
matthewkusto <mkusto.2001@gmail.com>
2024-01-31 23:28:07 -0800

    worked on the functions

--- Unit-2/Assignment 2/Bits.cpp
+++ Unit-2/Assignment 2/Bits.cpp
@@ -1,2 +1,84 @@
+#include "BitsByte.h"
 #include "BitsWord.h"
 
+/**
+ * @brief Construct a new Bits Word:: Bits Word object
+ *
+ */
+BitsWord::BitsWord() : BitsWord(0) {}
+
+/**
+ * @brief Construct a new Bits Word:: Bits Word object
+ *
+ * @param value
+ */
+BitsWord::BitsWord(int value) : mLower(nullptr), mUpper(nullptr)
+{
+    mUpper = new BitsByte();
+    mUpper->setValue(value >> 8);
+    mLower = new BitsByte();
+    mLower->setValue(value);
+}
+
+/**
+ * @brief
+ *
+ * @return BitsByte*
+ */
+BitsByte *BitsWord::getUpper() const
+{
+    return mUpper;
+};
+
+/**
+ * @brief
+ *
+ * @return BitsByte*
+ */
+BitsByte *BitsWord::getLower() const
+{
+    return mUpper;
+};
+
+/**
+ * @brief
+ *
+ * @return string
+ */
+string BitsWord::toString() const
+{
+    for (int i = 7; i >= 0; i--)
+    {
+        
+    }
+    
+}
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+int BitsWord::toInt() const
+{
+    int i = stoi(toString(), nullptr, 2);
+    return i;
+}
+
+/**
+ * @brief Destroy the Bits Word::Bits Word object
+ *
+ */
+BitsWord::~BitsWord()
+{
+    if (mLower != nullptr)
+    {
+
+        delete mLower;
+    }
+
+    if (mUpper != nullptr)
+    {
+        delete mUpper;
+    }
+}

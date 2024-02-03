d23d687f02d287263e0cc4ea62025bfad33dce0f
matthewkusto <mkusto.2001@gmail.com>
2024-02-02 18:11:31 -0800

    new doxygen comments

--- Unit-2/Assignment 2/Bytes.cpp
+++ Unit-2/Assignment 2/Bytes.cpp
@@ -7,55 +7,27 @@ Date: 01/27/2024
 
 #include "BitsByte.h"
 
-// #################################################
-//  @par Name
-//      BitsByte()
-//  @purpose
-//      Singleton that returns the instance of the class
-//  @param [in] :
-//      parameters are 0
-//  @return
-//      The instance of the class
-//  @par References
-//      None
-//  @par Notes
-//      Default constructor
-// ###############################################
+/**
+ * @brief Construct a new Bits Byte:: Bits Byte object
+ *
+ */
 BitsByte::BitsByte() : BitsByte(0) {}
 
-// #################################################
-//  @par Name
-//      BitsByte(int val)
-//  @purpose
-//      set the `val` param to the bits (binary code)
-//  @param [in] :
-//      an interger named val
-//  @return
-//      None
-//  @par References
-//      None
-//  @par Notes
-//      defualt function if an interger is entered
-// ###############################################
+/**
+ * @brief Construct a new Bits Byte:: Bits Byte object
+ *
+ * @param val
+ */
 BitsByte::BitsByte(int val)
 {
     this->setValue(val);
 }
 
-// #################################################
-//  @par Name
-//      BitsByte(int* ar)
-//  @purpose
-//      Sets the data section with the value found in the array//
-//  @param [in] :
-//      an array that points to the memory address
-//  @return
-//      None
-//  @par References
-//      points to the array that is in this function
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief Construct a new Bits Byte:: Bits Byte object
+ *
+ * @param ar
+ */
 BitsByte::BitsByte(int *ar)
 {
     for (int i = 0; i < 8; i++)
@@ -64,20 +36,11 @@ BitsByte::BitsByte(int *ar)
     }
 }
 
-// #################################################
-//  @par Name
-//      BitsByte(string val)
-//  @purpose
-//      Sets the value of the data section to the value found in val
-//  @param [in] :
-//      a string named val
-//  @return
-//      None
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief Construct a new Bits Byte:: Bits Byte object
+ *
+ * @param val
+ */
 BitsByte::BitsByte(string val)
 {
     for (int i = 0; i < 8; i++)
@@ -86,20 +49,11 @@ BitsByte::BitsByte(string val)
     }
 }
 
-// #################################################
-//  @par Name
-//      bitsToInt()
-//  @purpose
-//      Converts the bits from the array `bits` into an int
-//  @param [in] :
-//      None
-//  @return
-//      returns the int
-//  @par References
-//      const
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @return int
+ */
 int BitsByte::bitsToInt() const
 {
     int result = 0;
@@ -111,20 +65,11 @@ int BitsByte::bitsToInt() const
     return result;
 }
 
-// #################################################
-//  @par Name
-//      setValue(int)
-//  @purpose
-//      sets the value of the bits to either 1 or 0
-//  @param [in] :
-//      an interger named value
-//  @return
-//      None, a void function
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param value
+ */
 void BitsByte::setValue(int value)
 {
     int mask = 1;
@@ -135,39 +80,22 @@ void BitsByte::setValue(int value)
     }
 }
 
-// #################################################
-//  @par Name
-//      bitsToInt()
-//  @purpose
-//      find the location of the bit in the `bits` array
-//  @param [in] :
-//      takes in the index number for locating the bit
-//  @return
-//      returns the bit location
-//  @par References
-//      const
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param index
+ * @return int
+ */
 int BitsByte::at(int index) const
 {
     return bits[index];
 }
 
-// #################################################
-//  @par Name
-//      toString()
-//  @purpose
-//      to convert the binary (1's & 0's) from the array into a string
-//  @param [in] :
-//      None
-//  @return
-//      returns the string
-//  @par References
-//      const
-//  @par Notes
-//      return array as string
-// ###############################################
+/**
+ * @brief
+ *
+ * @return string
+ */
 string BitsByte::toString()
 {
     string _bits;
@@ -186,97 +114,56 @@ string BitsByte::toString()
     return _bits;
 }
 
-// #################################################
-//  @par Name
-//      toInt()
-//  @purpose
-//      calls the bitsToInt() function to bring back the int intitally input
-//  @param [in] :
-//      None
-//  @return
-//      returns the int
-//  @par References
-//      const
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @return int
+ */
 int BitsByte::toInt()
 {
     // int i = stoi(toString(), nullptr, 2);
     return bitsToInt();
 }
 
-// #################################################
-//  @par Name
-//      add(int adding)
-//  @purpose
-//      if the user want to add to the setValue(), they can do so
-//  @param [in] :
-//      an int adding
-//  @return
-//      returns the updated number after being added (it updates the bits as well)
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param adding
+ * @return int
+ */
 int BitsByte::add(int adding)
 {
     return bitsToInt() + adding;
 }
 
-// #################################################
-//  @par Name
-//      sub(int subtracting)
-//  @purpose
-//      if the user want to subtract from the setValue(), they can do so
-//  @param [in] :
-//      an int subtracting
-//  @return
-//      returns the updated number after being subtracted (it updates the bits as well)
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param subtracting
+ * @return int
+ */
 int BitsByte::sub(int subtracting)
 {
     return bitsToInt() - subtracting;
 }
 
-// #################################################
-//  @par Name
-//      mul(int multiplying)
-//  @purpose
-//      if the user want to multply from the setValue(), they can do so
-//  @param [in] :
-//      an int multiplying
-//  @return
-//      returns the updated number after being multiplied (it updates the bits as well)
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param multiplying
+ * @return int
+ */
 int BitsByte::mul(int multiplying)
 {
     return bitsToInt() * multiplying;
 }
 
-// #################################################
-//  @par Name
-//      div(int dividing)
-//  @purpose
-//      if the user want to divide from the setValue(), they can do so
-//  @param [in] :
-//      an int dividing
-//  @return
-//      returns the updated number after being divided (it updates the bits as well)
-//  @par References
-//      None
-//  @par Notes
-//      None
-// ###############################################
+/**
+ * @brief
+ *
+ * @param dividing
+ * @return int
+ */
 int BitsByte::div(int dividing)
 {
     return bitsToInt() / dividing;

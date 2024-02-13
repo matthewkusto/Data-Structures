973b2567b30ea2ffa53ca030d7d1b976f821395a
matthewkusto <mkusto.2001@gmail.com>
2024-02-12 20:57:04 -0800

    code setup

--- Unit-4/main.cpp
+++ Unit-4/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include "BitsByte.h"
 #include "BitsWord.h"
 using namespace std;
 
 int main()
 {
-    vector<string> vectWord;
-    vector<int> vectByte;
+    vector<BitsByte *> vectByte;
+    vector<BitsWord *> vectWord;
+    fstream myFile;
+
+    myFile.open("numbers(2).txt", ios::in);
+    if (myFile.is_open())
+    {
+        string line;
+        /* code */
+    }
 
     return 0;
 }
